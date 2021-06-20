eb7d3dec7b02acc0b64279c6876ead51b75e10a9
blustro <brunolustro@gmail.com>
2021-06-20 17:14:45 +0100

    Improving the form

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   $.get("../data.json", (data) => {
     const formItems = $.map(data.forms, (form, i) => {
       return `
-							<div class="col-12 col-lg-4">	
+							<div class="col-12 col-lg-4">
 								<label class="form-label">${form.name}</label>
 								<select class="form-select mb-3" aria-label="Default select example">
 									<option>Please select...</option>
@@ -16,21 +16,31 @@ $(document).ready(function () {
 
 								<form id="form${i + 1}" class="requires-validation" novalidate>
 									<div class="hidden">
-										<label class="form-label">Name</label>
-										<input type="text" name="name" class="form-control" placeholder="Your Name" required data-v-min-length="3">
+                    <div class="form-check">
+                      <label class="form-label">Name</label>
+										  <input type="text" name="name" class="form-control" placeholder="Your Name" required>
+                    </div>
+										
+                    <div class="form-check">
+                      <label class="form-label">E-mail</label>
+										  <input type="email" name="email" class="form-control" placeholder="email@email.com" required email>
+                    
+                    </div>
 
-										<label class="form-label">E-mail</label>
-										<input type="email" name="email" class="form-control" placeholder="email@email.com" required>
+                    <div class="form-check">
+					  					<label class="form-label">Phone</label>
+						  				<input type="text" name="phone" class="form-control" placeholder="Your phone number" required>
+                    </div>
 
-										<label class="form-label">Phone</label>
-										<input type="text" name="phone" class="form-control" placeholder="Your phone number" required>
+										<div class="form-check">
+                      <label class="form-label">Message</label>
+                      <textarea class="form-control"
+                      name="message"
+                      placeholder="Your message" required></textarea>
+                    </div>
 
-										<label class="form-label">Message</label>
-										<textarea class="form-control"
-										name="message"
-										placeholder="Your message" required></textarea>
-
-										<label class="form-label">Contact Preference</label required>
+										<div class="form-check">
+                    <label class="form-label">Contact Preference</label required>
 										<div class="col-12">
 											${form.contact
                         .map((pref, i) => {
@@ -51,6 +61,8 @@ $(document).ready(function () {
                         })
                         .join("")}
 											</div>
+                    </div>
+
 										<button class="btn btn-primary mt-3">Send</button>
 									</form>
 								</div>
@@ -59,39 +71,82 @@ $(document).ready(function () {
     }).join("");
     $formsContainer.append(formItems);
 
-    $("select").on("change", function () {
-      $(this).parent().find(".hidden").addClass("show");
+    $("select").on("change", function (e) {
+      const alert = document.querySelector(".alert");
+
+      e.target.parentNode.querySelector(".hidden").classList.add("show");
+      e.target.parentNode.contains(alert) ? alert.remove() : "";
     });
 
-    // $("form").on("submit", function (e) {
-    //   e.preventDefault();
-    //   var queryString = $(this).serializeArray();
-    //   $(this).addClass("was-validated");
-
-    //   console.log(queryString);
-    // });
-
-    const forms = document.querySelectorAll(".requires-validation");
-    Array.from(forms).forEach(function (form) {
+    function replaceValidationUI(form) {
       form.addEventListener(
-        "submit",
+        "invalid",
         function (event) {
           event.preventDefault();
-          event.stopPropagation();
-
-          const formTemplate = $(
-            '<div class="alert alert-success mt-3" role="alert">Form sent successfully.</div>'
-          );
-
-          form.classList.add("was-validated");
-
-          if (form.checkValidity()) {
-            formTemplate.appendTo(form);
-          }
         },
-        false
+        true
       );
-    });
+
+      form.addEventListener("submit", function (event) {
+        let sel = form.parentNode.querySelector("select");
+        let formNameValue = sel.options[sel.selectedIndex].text;
+
+        event.preventDefault();
+        event.stopPropagation();
+
+        if (this.checkValidity()) {
+          const formTemplate = $(
+            `<div class="alert alert-success alert-dismissible fade show mt-3" role="alert">
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+
+              Form sent successfully.
+            </div>`
+          );
+          const data = $(this).serializeArray();
+          data[data.length] = { formName: formNameValue };
+          console.log(data);
+
+          formTemplate.appendTo(form);
+
+          form.reset();
+          form.classList.remove("was-validated");
+          form.querySelector(".hidden").classList.remove("show");
+        }
+      });
+
+      var submitButton = form.querySelector(
+        "button:not([type=button]), input[type=submit]"
+      );
+      submitButton.addEventListener("click", function (event) {
+        let invalidFields = form.querySelectorAll(":invalid"),
+          errorMessages = form.querySelectorAll(".invalid-feedback"),
+          parent;
+
+        for (var i = 0; i < errorMessages.length; i++) {
+          errorMessages[i].parentNode.removeChild(errorMessages[i]);
+        }
+
+        for (var i = 0; i < invalidFields.length; i++) {
+          parent = invalidFields[i].closest(".form-check");
+          parent.insertAdjacentHTML(
+            "beforeend",
+            `<div class='invalid-feedback'>
+              ${invalidFields[i].validationMessage}
+            </div>`
+          );
+        }
+
+        if (invalidFields.length > 0) {
+          invalidFields[0].focus();
+          form.classList.add("was-validated");
+        }
+      });
+    }
+
+    const forms = document.querySelectorAll("form");
+    for (var i = 0; i < forms.length; i++) {
+      replaceValidationUI(forms[i]);
+    }
   });
 });
 
